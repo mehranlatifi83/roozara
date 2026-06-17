9b692ba5d890b866abc6ebe1c1d30272a1b8b17d
Mehran latifi <Mehran.latifi8383@gmail.com>
2026-06-17 21:01:59 +0330

    fix: correct Jalali calendar epoch from year 0 to year 1600 CE

--- app/src/main/java/ir/mehranlatifi83/helth/JalaliCalendar.java
+++ app/src/main/java/ir/mehranlatifi83/helth/JalaliCalendar.java
@@ -2,19 +2,24 @@ package ir.mehranlatifi83.helth;
 
 public class JalaliCalendar {
 
+    private static final int[] G_DAYS_IN_MONTH = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    private static final int[] J_DAYS_IN_MONTH = {31, 31, 31, 31, 31, 31, 30, 30, 30, 30, 30, 29};
+
     public static int[] toJalali(int gy, int gm, int gd) {
-        int[] gDayNo = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int gY = gy - 1600;
+        int gM = gm - 1;
+        int gD = gd - 1;
 
-        int gy2 = (gm > 2) ? gy + 1 : gy;
-        int gDayCount = 365 * (gy - 1)
-                + (gy2 + 3) / 4
-                - (gy2 + 99) / 100
-                + (gy2 + 399) / 400;
-        for (int i = 1; i < gm; i++) gDayCount += gDayNo[i];
-        if (gm > 2 && isGregorianLeap(gy)) gDayCount++;
-        gDayCount += gd - 1;
+        int gDayNo = 365 * gY
+                + (gY + 3) / 4
+                - (gY + 99) / 100
+                + (gY + 399) / 400;
 
-        int jDayNo = gDayCount - 79;
+        for (int i = 0; i < gM; i++) gDayNo += G_DAYS_IN_MONTH[i];
+        if (gM > 1 && isGregorianLeap(gy)) gDayNo++;
+        gDayNo += gD;
+
+        int jDayNo = gDayNo - 79;
         int jNp = jDayNo / 12053;
         jDayNo %= 12053;
 
@@ -26,11 +31,9 @@ public class JalaliCalendar {
             jDayNo = (jDayNo - 1) % 365;
         }
 
-        int[] jDayNoArr = {31, 31, 31, 31, 31, 31, 30, 30, 30, 30, 30, 29};
         int jm = 0;
-        for (int i = 0; i < 11 && jDayNo >= jDayNoArr[i]; i++) {
-            jDayNo -= jDayNoArr[i];
-            jm++;
+        for (; jm < 11 && jDayNo >= J_DAYS_IN_MONTH[jm]; jm++) {
+            jDayNo -= J_DAYS_IN_MONTH[jm];
         }
 
         return new int[]{jy, jm + 1, jDayNo + 1};
